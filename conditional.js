dc3f60c2e8eaf620cc12de6aa72854cb1b31881c
Arisandi <arisandisatria541@gmail.com>
2023-03-30 22:44:20 +0700

    added conditional, looping, and function exercise

---
+++ conditional.js
@@ -0,0 +1,97 @@
+// 1
+
+const person = "HR";
+
+let job = "";
+
+if (person === "HR") {
+  job += "Tugas saya adalah melakukan rekrutmen untuk calon pegawai baru.";
+} else if (person === "OB") {
+  job += "Tugas saya adalah menjaga kebersihan dan keindahan kantor.";
+} else if (person === "Security") {
+  job += "Tugas saya adalah menjaga keamanan kantor.";
+} else {
+  job += "Bukan bagian dari kantor";
+}
+
+// console.log(job);
+
+// No 2
+
+let a = "";
+let b = 2;
+
+let hasil = "";
+
+if (a == "" || b == "") {
+  hasil += "Input tidak boleh kosong!";
+} else {
+  if (a > b) {
+    hasil += `angka ${a} lebih besar dari angka ${b}`;
+  } else if (a < b) {
+    hasil += `angka ${a} lebih kecil dari angka ${b}`;
+  } else if (a === b) {
+    hasil += `angka ${a} sama dengan angka ${b}`;
+  } else {
+    hasil += "Input error";
+  }
+}
+
+// console.log(hasil);
+
+// No 3
+
+let number = 9;
+
+let day = "";
+
+switch (number) {
+  case 1:
+    day += "Minggu";
+    break;
+  case 2:
+    day += "Senin";
+    break;
+  case 3:
+    day += "Selasa";
+    break;
+  case 4:
+    day += "Rabu";
+    break;
+  case 5:
+    day += "Kamis";
+    break;
+  case 6:
+    day += "Jumat";
+    break;
+  case 7:
+    day += "Sabtu";
+    break;
+  default:
+    day += "Masukkan angka antara 1 sampai 7 saja!";
+}
+
+// console.log(day);
+
+// No 4
+
+let input = "UP";
+
+let charMovement = "";
+
+switch (input) {
+  case "UP":
+    charMovement += "Karakter berjalan keatas";
+    break;
+  case "RIGHT":
+    charMovement += "Karakter berjalan kekanan";
+    break;
+  case "BOTTOM":
+    charMovement += "Karakter berjalan kebawah";
+    break;
+  case "LEFT":
+    charMovement += "Karakter berjalan kekiri";
+    break;
+}
+
+console.log(charMovement);
---
+++ looping/looping.js
@@ -0,0 +1,61 @@
+const hasil = document.getElementById("hasil");
+
+// No 1
+
+let userCount = 100;
+
+let user = "";
+
+for (let i = 1; i <= userCount; i++) {
+  user += `<p>User ke - ${i} </p>`;
+}
+
+// hasil.innerHTML = user;
+
+// No 2
+
+let firstNumber = 0;
+
+for (let i = 0; i < 10; i++) {
+  firstNumber += 2;
+  // hasil.innerHTML += `<p>${firstNumber}</p>`;
+}
+
+// No 3
+
+let result = "";
+
+for (let i = 0; i < 20; i++) {
+  if (i % 2 === 0) {
+    result += `<p>Angka ${i} adalah GENAP</p>`;
+  } else if (i % 2 !== 0) {
+    result += `<p>Angka ${i} adalah GANJIL</p>`;
+  }
+}
+
+// hasil.innerHTML = result;
+
+// No 4
+
+// let count = 0;
+// let again = true;
+
+// while (again) {
+//   let question = confirm("Apakah anda mau mengulang?");
+//   question ? (count += 1) : (again = false);
+// }
+
+// hasil.innerHTML = `Perulangan sudah dilakukan sebanyak ${count} kali`;
+
+// No 5
+
+let quiz = true;
+
+while (quiz) {
+  let question = prompt("Sebutkan kepanjangan dari nama IB!");
+
+  if (question == "Impact Byte" || question == "impact byte") {
+    alert("Selamat jawaban kamu benar");
+    quiz = false;
+  }
+}
